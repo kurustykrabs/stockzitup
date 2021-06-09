d64ad8b88bd2c46afe0570d5f2e7fea289bc537a
kurustykrabs <kmijiphotos@gmail.com>
2021-06-10 01:24:11 +0800

    added dependent: :destroy on has_many models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :grocery_list_products
+  has_many :grocery_list_products, dependent: :destroy
   has_many_attached :images, dependent: :destroy
   has_many :grocery_lists, through: :grocery_list_products
 end
