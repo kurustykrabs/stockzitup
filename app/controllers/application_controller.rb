e531ace343c56bb27eb276ec02e6c39e54b27d5a
kurustykrabs <kmijiphotos@gmail.com>
2021-06-09 10:02:32 +0800

    route login page to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource_or_scope)
+  home_index_path
+  end
+
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :user
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  # root to: "home#index"
+  resources :home
   resources :products
   resources :categories
   resources :grocery_lists
