11b84873afdce99312647c9885f4fe043ce95895
kurustykrabs <kmijiphotos@gmail.com>
2021-06-09 12:17:39 +0800

    added lines in application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Prevent initializing the application before assets are precompiled (required for heroku)
+config.assets.initialize_on_precompile = false
+# Add Rails Admin assets (required)
+config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+
 module Stockzitup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
