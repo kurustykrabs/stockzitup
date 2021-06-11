d1a6fca43e522d92508ac746e20835b847704804
kurustykrabs <kmijiphotos@gmail.com>
2021-06-11 15:19:23 +0800

    fixed subtotal

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -3,6 +3,8 @@ class GroceryList < ApplicationRecord
   has_many :grocery_list_products, dependent: :destroy
   has_many :products, through: :grocery_list_products
 
+  before_save :set_subtotal
+
   def subtotal
     grocery_list_products.collect { |grocery_list_product| grocery_list_product.valid? ? grocery_list_product.unit_price * grocery_list_product.quantity : 0 }.sum
   end
