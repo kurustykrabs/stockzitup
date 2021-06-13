fdedbd8cdc5738f5f6c767035518628d89315756
kurustykrabs <kmijiphotos@gmail.com>
2021-06-13 23:34:39 +0800

    added some lines in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -79,6 +83,9 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { :host => 'https://whispering-atoll-46703.herokuapp.com/' }
+
+  Rails.application.routes.default_url_options[:host] = 'https://whispering-atoll-46703.herokuapp.com/'
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
