36c14ed138bf5dfa5118c6a45efa6e90c7653e81
kurustykrabs <kmijiphotos@gmail.com>
2021-06-13 22:41:44 +0800

    comment back in schema

--- db/migrate/20210613070952_add_confirmable_to_devise.rb
+++ db/migrate/20210613070952_add_confirmable_to_devise.rb
@@ -11,6 +11,6 @@ class AddConfirmableToDevise < ActiveRecord::Migration[6.1]
   def down
     remove_index :users, :confirmation_token
     remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
-    # remove_columns :users, :unconfirmed_email
+    remove_columns :users, :unconfirmed_email
   end
 end
